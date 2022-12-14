c7151e947afab7c60d40a480455e8f6398784ee2
Himandri Sharma <himandrisharma27@gmail.com>
2022-12-14 21:28:45 +0530

    chaning for deployment

--- controllers/calculator.js
+++ controllers/calculator.js
@@ -1,25 +1,28 @@
-
-
-module.exports.add = (req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let sum = n1 + n2;
-    res.send(sum);
+module.exports.add = (req, res) => {
+	let n1 = req.body.num1;
+	let n2 = req.body.num2;
+	let sum = n1 + n2;
+	// res.send(sum);
+    res.json({
+        result : sum
+    })
+	// return sum;
 };
 
-module.exports.subtract = (req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let diff = n1 - n2;
-    res.send(diff);
+module.exports.subtract = (req, res) => {
+	let n1 = req.body.num1;
+	let n2 = req.body.num2;
+	let diff = n1 - n2;
+	res.json({
+        result : diff
+    });
 };
 
-module.exports.multiply = (req, res)=>{
-
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let product = n1 * n2;
-    res.send(product);
+module.exports.multiply = (req, res) => {
+	let n1 = req.body.num1;
+	let n2 = req.body.num2;
+	let product = n1 * n2;
+	res.json({
+        result : product
+    });
 };
